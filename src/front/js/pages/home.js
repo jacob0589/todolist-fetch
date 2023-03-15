3664769b31ad17478af077ec96311ef03745864f
jacob0589 <alonsocb8995@gmail.com>
2023-03-15 01:36:23 +0000

    avance

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,60 +4,82 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const [arregloTemp, setArregloTemp] = useState([
-		{ tarea: "Supermercado", done: false },
-		{ tarea: "Meal Prep", done: false }
-	]);
+  const { store, actions } = useContext(Context);
+ const [arregloTemp, setArregloTemp] = useState([]);
+ useEffect(() => {
+    const cargaDeDatos = async () => {
+      let { respuestaJson, response } = await actions.useFetch( "todos/user/jacob")
+      if (response.ok) {
+        setArregloTemp(respuestaJson)
+      }
+    }
+    cargaDeDatos()
+  }, [])
 
-	const eliminarTarea = (indice) => {
-		setArregloTemp(
-			arregloTemp.filter((item, index) => {
-				return index !==indice;
-			})
-		)
-	}
+  useEffect(() => {}, [arregloTemp])
 
-	useEffect(() => { console.log("se renderizó el componente") }, [arregloTemp]);
 
-	return (
-		<div className="container-fluid justify-content-center align-item-center ">
-			<div className="row d-flex justify-content-center m-5 p-5">
-				<div className="row display-1 text-danger d-flex justify-content-center">
-					Todos
-				</div>
-				<input
-					placeholder="What needs to be done?"
-					onKeyDown={(e) => { //event listener
-						if (e.keyCode == "13") { //el 13 significa enter
-							//console.log("Presionaste el enter")
-							setArregloTemp([...arregloTemp, { tarea: e.target.value, done: false }])
-						}
-					}
-					}
-				/>
-				{arregloTemp && arregloTemp.length > 0 ?
-					<>{arregloTemp.map((item, index) => {
-						return <li key={index} className="d-flex justify-content-between border m-0">
-							{item.tarea}
-							<p
-								className="ocultar text-danger"
-								type="button"
-								onClick={() => {
-									eliminarTarea(index);
-								}}
-							>
-								X
-							</p>
-						</li>
-					})}</>
-					:
-					<><h1>NO hay tareas</h1></>}
-				<div className="fs-6 border"> {arregloTemp.length} Item left </div>
-				<div className="relleno1 shadow fs-6 border">
-				</div>
-				<div className="relleno2 shadow fs-6 border">
-				</div>
-			</div>
-		</div>
-	);
+
+  const eliminarTarea = (indice) => {
+    setArregloTemp(
+      arregloTemp.filter((item, index) => {
+        return index !== indice;
+      })
+    );
+  };
+
+  
+  return (
+    <div className="container-fluid justify-content-center align-item-center ">
+      <div className="row d-flex justify-content-center m-5 p-5">
+        <div className="row display-1 text-danger d-flex justify-content-center">
+          Todos
+        </div>
+        <input
+          placeholder="What needs to be done?"
+          onKeyDown={(e) => {
+            //event listener
+            if (e.keyCode == "13") {
+              //el 13 significa enter
+              //console.log("Presionaste el enter")
+              setArregloTemp([
+                ...arregloTemp,
+                { label: e.target.value, done: false },
+              ]);
+            }
+          }}
+        />
+        {arregloTemp && arregloTemp.length > 0 ? (
+          <>
+            {arregloTemp.map((item, index) => {
+              return (
+                <li
+                  key={index}
+                  className="d-flex justify-content-between border m-0"
+                >
+                  {item.label}
+                  <p
+                    className="ocultar text-danger"
+                    type="button"
+                    onClick={() => {
+                      eliminarTarea(index);
+                    }}
+                  >
+                    X
+                  </p>
+                </li>
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <h1>NO hay tareas</h1>
+          </>
+        )}
+        <div className="fs-6 border"> {arregloTemp.length} Item left </div>
+        <div className="relleno1 shadow fs-6 border"></div>
+        <div className="relleno2 shadow fs-6 border"></div>
+      </div>
+    </div>
+  );
 };
